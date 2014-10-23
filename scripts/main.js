47523685824316dc6b106d2a224ed2f2602ae190
Tim Gallant <me@timgallant.us>
2014-10-22 23:38:52 -0700

    adding new post, adding js for modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,67 @@
+//main javasacript file for maaxcloud landing page
+/* onload function that sets event listeners and initial state*/
+window.onload = function () {
+  //sets focused field in modal
+  $('#emailPopup').on('shown.bs.modal', function() {
+    if(document.getElementById('address').value != 0) {
+      document.getElementById('modal-name').focus();
+    }
+    else {
+      document.getElementById('modal-address').focus();
+    }
+  });
+
+  //submits modal form w/o reloading the page
+  $('#sub-form').submit(function () {
+    var email = document.getElementById('modal-address');
+    var name = document.getElementById('modal-name');
+    var message = document.getElementById('modal-message');
+
+    var formData = {
+      'email': email.value,
+      'name': name.value,
+      'message': message.value
+    };
+
+    //ajax for submitting form
+    $.ajax({
+      url: '/mail',
+      type: 'POST',
+      contentType: "application/json",
+      data: JSON.stringify(formData),
+      success: function () {
+        document.getElementById('modal-body').innerHTML = "<div><h1>Thank you!</h1></div>"
+          + "<button class='btn btn-primary' onclick='popupClose();'>Close</button>";
+      },
+      error: function () {
+        document.getElementById('modal-body').innerHTML = "<p>Sorry, there was a problem in submitting your request. "
+          + "Please try again, but if the problem persists, send us an email at info@privy.io<p>";
+      }
+    });
+  });
+};
+
+//opens modal window
+showModal = function() {
+  var modalWindow = document.getElementById('emailPopup');
+  var email = document.getElementById('address');
+  var modalEmail = document.getElementById('modal-address');
+  modalEmail.value = email.value;
+  $('#emailPopup').modal('show');
+};
+
+//closes modal window
+popupClose = function() {
+  $('#emailPopup').modal('hide');
+};
+
 //this is where we apply opacity to the arrow
 $(window).scroll( function(){
 
   //get scroll position
   var topWindow = $(window).scrollTop();
   //multipl by 1.5 so the arrow will become transparent half-way up the page
-  var topWindow = topWindow * 1.5;
+  topWindow = topWindow * 1.5;
   
   //get height of window
   var windowHeight = $(window).height();
@@ -20,20 +77,15 @@ $(window).scroll( function(){
 
 });
 
-
-
-
-
-
 //Code stolen from css-tricks for smooth scrolling:
 
 $(document).ready(function() {
 
   function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
+    return string
+      .replace(/^\//,'')
+      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+      .replace(/\/$/,'');
   }
   var locationPath = filterPath(location.pathname);
   var scrollElem = scrollableElement('html', 'body');
@@ -41,8 +93,8 @@ $(document).ready(function() {
   $('a[href*=#]').each(function() {
     var thisPath = filterPath(this.pathname) || locationPath;
     if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
+          && (location.hostname == this.hostname || !this.hostname)
+          && this.hash.replace(/#/,'') ) {
       var $target = $(this.hash), target = this.hash;
       if (target) {
         var targetOffset = $target.offset().top;
@@ -79,62 +131,8 @@ $(document).ready(function() {
 
 // Expand fntn
 $('.row .btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $collapse = $this.closest('.collapse-group').find('.collapse');
-    $collapse.collapse('toggle');
+  e.preventDefault();
+  var $this = $(this);
+  var $collapse = $this.closest('.collapse-group').find('.collapse');
+  $collapse.collapse('toggle');
 });
-
-
-
-// More
-
-window.onload = function () {
-    $('#myModal').on('shown.bs.modal', function() {
-        if(document.getElementById('emailInput1').value != 0) {
-            document.getElementById('modal-name').focus();
-        }
-        else {
-            document.getElementById('modal-email').focus();
-        }
-    });
-    
-    $('#modal-form').submit(function () {
-        var email = document.getElementById('modal-email');
-        var name = document.getElementById('modal-name');
-        var message = document.getElementById('modal-message');
-
-        var formData = {
-            'email': email.value,
-            'name': name.value,
-            'message': message.value
-        };
-
-        $.ajax({
-            url: '/',
-            type: 'POST',
-            contentType: "application/json",
-            data: JSON.stringify(formData),
-            success: function () {
-                //$('#myModal').modal('hide');
-                document.getElementById('modal-body').innerHTML = "<div><h1>Thank you!</h1></div><button class='btn btn-primary' onclick='popupClose();'>Close</button>";
-            },
-            error: function () {
-                document.getElementById('modal-body').innerHTML = "<p>Sorry, there was a problem in submitting your request. Please try again, but if the problem persists, send us an email at info@privy.io<p>";
-            }
-        });
-    });
-};
-
-popupOpen = function() {
-    var modalWindow = document.getElementById('myModal');
-    var email = document.getElementById('emailInput1');
-    var modalEmail = document.getElementById('modal-email');
-
-    modalEmail.value = email.value;
-    $('#myModal').modal('show');
-};
-
-popupClose = function() {
-    $('#myModal').modal('hide');
-};
